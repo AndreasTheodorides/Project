54ef73ccf05c98ee2e9a8b28c7cd778cc883c55a
Andreas Theodoridis <antreastheodorides16@gmail.com>
2021-03-10 10:48:59 +0200

    Initial commit

---
+++ app/src/main/java/com/unipi/atheodoridis/nfccardapp/SignInActivity.java
@@ -0,0 +1,27 @@
+package com.unipi.atheodoridis.nfccardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class SignInActivity extends AppCompatActivity {
+    EditText editText1,editText2;
+    ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+    }
+    public void signIn(View view){
+        Intent intent = new Intent(this,GetMyCard.class);
+        startActivity(intent);
+    }
+}
